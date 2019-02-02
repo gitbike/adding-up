69de88c980d539c86702578cab94785bbdd84d2a
gitbike <git_bike_840@yahoo.co.jp>
2019-02-02 19:30:50 +0900

    無名関数の中身を書き換え

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const readline = require('readline');
 const rs = fs.ReadStream('./popu-pref.csv');
 const rl = readline.createInterface({ 'input': rs, 'output': {} });
 rl.on('line', (lineString) => {
-  console.log(lineString);
+  const columns = lineString.split(',');
+  const year = parseInt(columns[0]);
+  const prefecture = columns[2]; 
+  const pop = parseInt(columns[7]);
+  if (year == 2010 || year == 2017) {
+    console.log(year);
+    console.log(prefecture);
+    console.log(pop);
+  }
 });
 rl.resume();
