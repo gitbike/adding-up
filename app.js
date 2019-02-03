fc2845c457fd8bf42bd8e101d5e37e16b155bb76
gitbike <git_bike_840@yahoo.co.jp>
2019-02-03 19:23:39 +0900

    mapメソッドで情報を取り出すように書き換え

--- app.js
+++ app.js
@@ -4,15 +4,32 @@ const fs = require('fs');
 const readline = require('readline');
 const rs = fs.ReadStream('./popu-pref.csv');
 const rl = readline.createInterface({ 'input': rs, 'output': {} });
+const map = new Map();  // key: 都道府県  value: 集計データのオブジェクト
+
 rl.on('line', (lineString) => {
   const columns = lineString.split(',');
   const year = parseInt(columns[0]);
   const prefecture = columns[2]; 
-  const pop = parseInt(columns[7]);
+  const popu = parseInt(columns[7]);
   if (year == 2010 || year == 2017) {
-    console.log(year);
-    console.log(prefecture);
-    console.log(pop);
+    let value = map.get(prefecture);
+    if (!value) {
+      value = {
+        popu10: 0,
+        popu15: 0,
+        change: null,
+      };
+    }
+    if (year === 2010) {
+      value.popu10 += popu;
+    }
+    if (year === 2015) {
+      value.popu15 += popu;
+    }
+    map.set(prefecture, value);
   }
 });
 rl.resume();
+rl.on('close', () => {
+  console.log(map);
+});
